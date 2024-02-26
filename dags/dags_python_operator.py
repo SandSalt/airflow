979669c215557c8bca27d42aa99be95c2bcd5d54
SandSalt <m.ybk102@gmail.com>
2024-02-26 18:49:30 +0900

    그냥

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -10,8 +10,7 @@ with DAG(
     schedule="0 0 * * *", #5개항목 분,시,일,월,요일,언제 돌지를 나타냄.
     start_date=pendulum.datetime(2024, 2, 1, tz="Asia/Seoul"), #tz="UTC"는 한국시간보다 9시간 더 뒤
     catchup=False, #True는 시작일시 이전 모두 돌리게 됨. 차례차례안돔. 한꺼번에 돔
-) as dag:
-    
+) as dag:    
     def select_fruit():
         fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
         rand_int = random.randint(0,3)
