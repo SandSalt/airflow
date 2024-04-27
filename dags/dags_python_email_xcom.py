df004ee7f902cb3e2b1c679efd886784d966a864
home <m.ybk102@gamil.com>
2024-04-27 21:02:33 +0900

    ㅋ 두자리가 아니네

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -7,7 +7,7 @@ from airflow.operators.email import EmailOperator
 with DAG(
     dag_id = "dags_python_email_xcom",
     schedule = "0 8 1 * *",
-    start_date = pendulum.datetime(2024, 01, 01, tz="Asia/Seoul"),
+    start_date = pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup = False
 ) as dag:
 
